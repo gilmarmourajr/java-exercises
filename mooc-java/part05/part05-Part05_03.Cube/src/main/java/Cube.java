62d2d904f0588811102602a56e4c3078d8996b24
gilmarmourajr <juzinho-gilmar@hotmail.com>
2025-03-03 20:37:25 -0300

    Add exercises 05-1 to 05-4

---
+++ mooc-java/part05/part05-Part05_03.Cube/src/main/java/Cube.java
@@ -0,0 +1,15 @@
+public class Cube {
+    private int edge;
+    
+    public Cube(int edgeLength){
+        this.edge = edgeLength;
+    }
+    
+    public int volume(){
+        return edge * edge * edge;
+    }
+    
+    public String toString(){
+        return "The length of the edge is "+this.edge+" and the volume is "+this.volume();
+    }
+}
