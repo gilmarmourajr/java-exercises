32a6d0c91e68754b14101b99b7e53d09a0ce2292
gilmarmourajr <juzinho-gilmar@hotmail.com>
2025-03-11 11:45:01 -0300

    Add ex 04

--- programacao-orientada-objetos-fiap/aula0311/src/Ex_04/Main.java
+++ programacao-orientada-objetos-fiap/aula0311/src/Ex_04/Main.java
@@ -1,16 +1,93 @@
 package Ex_04;
 
+import java.util.Scanner;
+
 public class Main {
+    static int numFuncionarios = 0;
     public static void main(String[] args) {
-        Funcionario fernando = new Funcionario("Fernando", 1500, "Desenvolvedor");
-        System.out.println(fernando);
+        Scanner sc = new Scanner(System.in);
+        Funcionario[] funcionarios = new Funcionario[2];
+        int selecao = 0;
 
-        fernando.aumentarSalario("Desenvolvedor", 50);
-        fernando.aumentarSalario("Designer", 50);
-        fernando.promover("Scrum Master");
-        System.out.println(fernando);
+        while(selecao != 4){
+            System.out.println("1 - Cadastrar funcionário");
+            System.out.println("2 - Pesquisar funcionário");
+            System.out.println("3 - Listar funcionários");
+            System.out.println("4 - Finalizar");
+            selecao = Integer.parseInt(sc.nextLine());
 
-        fernando.aumentarSalario(10);
-        System.out.println(fernando);
+            while(!(selecao>=1 && selecao<=4)){
+                System.out.println("Insira um número de 1 a 4.");
+                System.out.println("1 - Cadastrar funcionário");
+                System.out.println("2 - Pesquisar funcionário");
+                System.out.println("3 - Listar funcionários");
+                System.out.println("4 - Finalizar");
+                selecao = Integer.parseInt(sc.nextLine());
+            }
+            System.out.println();
+
+            if (selecao == 1) {
+                if(numFuncionarios < funcionarios.length){
+                    cadastrarFuncionario(funcionarios, sc);
+                    numFuncionarios++;
+                } else System.out.println("Capacidade máxima de cadastros atingida!");
+            }
+            if (selecao == 2) pesquisarFuncionario(funcionarios, sc);
+            if (selecao == 3) listarFuncionarios(funcionarios);
+        }
+        sc.close();
+    }
+
+    public static void cadastrarFuncionario(Funcionario[] f, Scanner sc){
+        System.out.print("Insira o nome do funcionário: ");
+        String nome = sc.nextLine();
+        System.out.print("Insira o cargo: ");
+        String cargo = sc.nextLine();
+        System.out.print("Insira o salário (em R$): ");
+        double salario = Double.parseDouble(sc.nextLine());
+        System.out.println();
+
+        f[numFuncionarios] = new Funcionario(nome, salario, cargo);
+        System.out.println(numFuncionarios);
+    }
+
+    public static void pesquisarFuncionario(Funcionario[] f, Scanner sc){
+        if(numFuncionarios == 0){
+            System.out.println("Nenhum funcionário cadastrado!");
+            return;
+        }
+
+        System.out.println("Insira o nome do funcionário a ser buscado: ");
+        String busca = sc.nextLine();
+        boolean encontrado = false;
+
+        if(busca.isEmpty()){
+            System.out.println("Insira o nome do funcionário a ser buscado: ");
+        }
+
+        for(int i=0; i<numFuncionarios; i++){
+            if(f[i].nome.equalsIgnoreCase(busca)){
+                System.out.println(f[i]);
+                encontrado = true;
+            }
+        }
+
+        if(!encontrado){
+            System.out.println("Nenhum resultado encontrado.");
+        }
+
+        System.out.println();
+    }
+
+    public static void listarFuncionarios(Funcionario[] f){
+        if(numFuncionarios == 0){
+            System.out.println("Nenhum funcionário cadastrado!");
+            return;
+        }
+
+        for(int i=0; i<numFuncionarios; i++){
+            System.out.println(f[i]);
+        }
+        System.out.println();
     }
 }
