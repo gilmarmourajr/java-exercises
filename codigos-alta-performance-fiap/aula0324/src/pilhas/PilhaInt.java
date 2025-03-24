bded7eaa73e0ed7ea7561ed38076c65cd31a37d9
gilmarmourajr <juzinho-gilmar@hotmail.com>
2025-03-24 11:50:32 -0300

    Add exercises 3 and 4

--- codigos-alta-performance-fiap/aula0324/src/pilhas/PilhaInt.java
+++ codigos-alta-performance-fiap/aula0324/src/pilhas/PilhaInt.java
@@ -2,29 +2,49 @@ package pilhas;
 
 public class PilhaInt {
 
-	final int N = 6;
-	int[] dados = new int[N];
-	int topo;
+    final int N = 7;
+    int[] dados = new int[N];
+    private int topo;
 
-	public void init() {
-		topo = 0;
+    public void init() {
+        topo = 0;
+    }
+
+    public boolean isFull() {
+        return (topo == N);
+    }
+
+    public boolean isEmpty() {
+        return (topo == 0);
+    }
+
+    public void push(int elem) {
+        if (isFull())
+            System.out.println("Stack overflow");
+        else {
+            dados[topo] = elem;
+            topo++;
+        }
+    }
+
+    public int pop() {
+        topo--;
+        return (dados[topo]);
+    }
+
+	public int top() {
+		if(topo == 0) {
+			System.out.println("Stack is empty!");
+			return -1;
+		}
+
+		return (dados[topo-1]);
 	}
 
-	public boolean isFull() {
-		return (topo == N);
-	}
-
-	public void push(int elem) {
-		if (isFull())
-			System.out.println("Stack overflow");
-		else {
-			dados[topo] = elem;
-			topo++;
+	public void clear() {
+		while(!(this.isEmpty())) {
+			System.out.print(this.pop() + " ");
 		}
 	}
-
-	public int pop() {
-		topo--;
-		return (dados[topo]);
-	}
 }
+
