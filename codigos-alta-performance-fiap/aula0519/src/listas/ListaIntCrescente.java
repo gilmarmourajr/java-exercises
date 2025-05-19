5b51271bc32d6e7e91fa24c789c47e15cff7124d
GilmarMouraJr <juzinho-gilmar@hotmail.com>
2025-05-19 10:39:38 -0300

    Add remove method to listaInt

--- codigos-alta-performance-fiap/aula0519/src/listas/ListaIntCrescente.java
+++ codigos-alta-performance-fiap/aula0519/src/listas/ListaIntCrescente.java
@@ -40,4 +40,33 @@ public class ListaIntCrescente {
 		}
 	}
 
+	public boolean remove(int valor) {
+		boolean achou = false;
+		if (lista != null) {
+			// verificar se o primeiro valor é o procurado
+			if (lista.dado == valor) {
+				lista = lista.prox;
+			} else {
+				NO aux = lista;
+				while (lista != null && !achou) {
+					if (aux.prox.dado == valor) {
+						achou = true;
+					} else {
+						aux = aux.prox;
+					}
+				}
+
+				if (achou) {
+					aux.prox = aux.prox.prox;
+				} else {
+					System.out.println("Valor nao encontrado");
+				}
+			}
+		} else {
+			System.out.println("Lista vazia");
+		}
+
+		return achou;
+	}
+
 }
